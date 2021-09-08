f303473db1a89ac01c5de467864fa98cac92f173
samarimam <samarimam78@gmail.com>
2021-09-08 23:25:16 +0530

    image upload

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,47 +1,55 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'http://localhost:8000';
+const url = "http://localhost:8000";
 
+export const uploadFile = async (data) => {
+    console.log(data);
+    try {
+        return await axios.post(`${url}/file/upload`, data);
+    } catch (error) {
+        console.log("Error while calling uploadFile API ", error);
+    }
+};
 
 export const createPost = async (post) => {
     try {
         return await axios.post(`${url}/create`, post);
     } catch (error) {
-        console.log('Error while calling createPost API ', error);
+        console.log("Error while calling createPost API ", error);
     }
-}
+};
 
 export const getAllPosts = async (param) => {
     try {
         let response = await axios.get(`${url}/posts${param}`);
+        console.log(param, "param value");
         return response.data;
     } catch (error) {
-        console.log('Error while calling getPosts API ', error)
+        console.log("Error while calling getPosts API ", error);
     }
-}
+};
 
 export const getPost = async (id) => {
     try {
         let response = await axios.get(`${url}/post/${id}`);
         return response.data;
     } catch (error) {
-        console.log('Error while calling getPost API ', error);
+        console.log("Error while calling getPost API ", error);
     }
-}
+};
 
 export const updatePost = async (id, post) => {
     try {
         return await axios.post(`${url}/update/${id}`, post);
-        
-    } catch(error) {
-        console.log('Error while calling updatePost API ', error)
+    } catch (error) {
+        console.log("Error while calling updatePost API ", error);
     }
-}
+};
 
 export const deletePost = async (id) => {
     try {
         return await axios.delete(`${url}/delete/${id}`);
-    } catch(error) {
-        console.log('Error while calling deletePost API ', error)
+    } catch (error) {
+        console.log("Error while calling deletePost API ", error);
     }
-}
+};
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import { createPost, getAllPosts, getPost,updatePost,deletePost} from '../controller/post-controller.js';
-
+import express from "express";
+import {
+    createPost,
+    getAllPosts,
+    getPost,
+    updatePost,
+    deletePost,
+} from "../controller/post-controller.js";
+import { uploadImage, getImage } from "../controller/image-controller.js";
+import upload from "../utils/upload.js";
 
 const router = express.Router();
 
+router.post("/create", createPost);
+router.get("/posts", getAllPosts);
+router.get("/post/:id", getPost);
+router.post("/update/:id", updatePost);
+router.delete("/delete/:id", deletePost);
+router.post("/file/upload", upload.single("file"), uploadImage);
+router.get("/file/:filename", getImage);
 
-
-router.post('/create', createPost);
-router.get('/posts',getAllPosts);
-router.get('/post/:id', getPost);
-router.post('/update/:id',updatePost);
-router.delete('/delete/:id', deletePost);
-
-
-export default router; 
+export default router;
