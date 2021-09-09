7c268bd616d2766ffdb9948012d1e033a40d54bb
samarimam <samarimam78@gmail.com>
2021-09-09 11:57:33 +0530

    authentication completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
-import Header from './components/Header';
-import Home from './components/home/Home';
-import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import React from 'react';
-import { Box } from '@material-ui/core';
-import DetailView from './components/post/DetailView';
-import CreateView from './components/post/CreateView';
-import UpdateView from './components/post/UpdateView';
-
+// import Header from './components/Header';
+// import Home from './components/home/Home';
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+// import { Box } from '@material-ui/core';
+// import DetailView from './components/post/DetailView';
+// import CreateView from './components/post/CreateView';
+// import UpdateView from './components/post/UpdateView';
+import AppWithRouterAccess from "./AppWithRouterAccess";
 
 function App() {
-  return (
-    <BrowserRouter>
-       <Header />
+    return (
+        <BrowserRouter>
+            <AppWithRouterAccess />
+            {/* <Header />
         <Box style={{marginTop: 64}}>
           <Switch>
             <Route exact path='/' component={Home} />
             {/* <DetailView /> */}
-            <Route exact path='/details/:id' component={DetailView} />
+            {/* <Route exact path='/details/:id' component={DetailView} />
             <Route exact path='/create' component={CreateView} />
             <Route exact path='/update/:id' component={UpdateView} />
           </Switch>
-        </Box>
-      </BrowserRouter>
-  );
+        </Box> */}
+        </BrowserRouter>
+    );
 }
 
 export default App;
