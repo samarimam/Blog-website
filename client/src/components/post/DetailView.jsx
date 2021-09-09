5324f16517c2fc22da4c711ef8e99d2c0e65ab4e
samarimam <samarimam78@gmail.com>
2021-09-09 16:29:13 +0530

    comment functionality added

--- client/src/components/post/DetailView.jsx
+++ client/src/components/post/DetailView.jsx
@@ -1,55 +1,57 @@
-import { useState, useEffect, useContext } from 'react';
-import { Box, makeStyles, Typography, Grid } from '@material-ui/core';
-import { Delete, Edit } from '@material-ui/icons';
-import { Link, useHistory } from 'react-router-dom'
-import { getPost,deletePost } from '../../service/api';
+import { useState, useEffect, useContext } from "react";
+import { Box, makeStyles, Typography, Grid } from "@material-ui/core";
+import { Delete, Edit } from "@material-ui/icons";
+import { Link, useHistory } from "react-router-dom";
+import { getPost, deletePost } from "../../service/api";
+import Comments from "../comments/comments";
 
-const useStyle = makeStyles(theme => ({
+const useStyle = makeStyles((theme) => ({
     container: {
-        margin: '50px 100px',
-        [theme.breakpoints.down('md')]: {
-            margin: 0
+        margin: "50px 100px",
+        [theme.breakpoints.down("md")]: {
+            margin: 0,
         },
     },
     image: {
-        width: '100%',
-        height: '50vh',
-        objectFit: 'cover'
+        width: "100%",
+        height: "50vh",
+        objectFit: "cover",
     },
     icons: {
-        float: 'right'
+        float: "right",
     },
     icon: {
         margin: 5,
         padding: 5,
-        border: '1px solid #878787',
-        borderRadius: 10
+        border: "1px solid #878787",
+        borderRadius: 10,
     },
     heading: {
         fontSize: 38,
         fontWeight: 600,
-        textAlign: 'center',
-        margin: '50px 0 10px 0'
+        textAlign: "center",
+        margin: "50px 0 10px 0",
     },
     author: {
-        color: '#878787',
-        display: 'flex',
-        margin: '20px 0',
-        [theme.breakpoints.down('sm')]: {
-            display: 'block'
+        color: "#878787",
+        display: "flex",
+        margin: "20px 0",
+        [theme.breakpoints.down("sm")]: {
+            display: "block",
         },
     },
     link: {
-        textDecoration: 'none',
-        color: 'inherit'
-    }
+        textDecoration: "none",
+        color: "inherit",
+    },
 }));
 
-const DetailView = ({match}) => {
+const DetailView = ({ match }) => {
     const classes = useStyle();
-    const url = 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
+    const url =
+        "https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80";
     const history = useHistory();
-    
+
     const [post, setPost] = useState({});
 
     useEffect(() => {
@@ -57,35 +59,57 @@ const DetailView = ({match}) => {
             let data = await getPost(match.params.id);
             console.log(data);
             setPost(data);
-        }
+        };
         fetchData();
         console.log(post.username);
     }, []);
 
-    const deleteBlog = async () => {    
+    const deleteBlog = async () => {
         await deletePost(post._id);
-        history.push('/')
-    }
+        history.push("/");
+    };
 
     return (
         <Box className={classes.container}>
-            <img src={post.picture || url} alt="post" className={classes.image} />
+            <img
+                src={post.picture || url}
+                alt='post'
+                className={classes.image}
+            />
             <Box className={classes.icons}>
-                        <Link to={`/update/${post._id}`}><Edit className={classes.icon} color="primary"/></Link>
-                        <Link><Delete onClick={() => deleteBlog()} className={classes.icon} color="error" /></Link>
+                <Link to={`/update/${post._id}`}>
+                    <Edit className={classes.icon} color='primary' />
+                </Link>
+                <Link>
+                    <Delete
+                        onClick={() => deleteBlog()}
+                        className={classes.icon}
+                        color='error'
+                    />
+                </Link>
             </Box>
-            
+
             <Typography className={classes.heading}>{post.title}</Typography>
             <Box className={classes.author}>
-            <Link to={`/?username=${post.username}`} className={classes.link}>
-                    <Typography>Author: <span style={{fontWeight: 600}}>{post.username}</span></Typography>
+                <Link
+                    to={`/?username=${post.username}`}
+                    className={classes.link}
+                >
+                    <Typography>
+                        Author:{" "}
+                        <span style={{ fontWeight: 600 }}>{post.username}</span>
+                    </Typography>
                 </Link>
-                <Typography style={{marginLeft: 'auto'}}>{new Date(post.createdDate).toDateString()}</Typography>
-
+                <Typography style={{ marginLeft: "auto" }}>
+                    {new Date(post.createdDate).toDateString()}
+                </Typography>
             </Box>
-            <Typography className={classes.detail}>{post.description}</Typography>
+            <Typography className={classes.detail}>
+                {post.description}
+            </Typography>
+            <Comments post={post} />
         </Box>
-    )
-}
+    );
+};
 
 export default DetailView;
--- server/routes/route.js
+++ server/routes/route.js
@@ -8,6 +8,11 @@ import {
 } from "../controller/post-controller.js";
 import { uploadImage, getImage } from "../controller/image-controller.js";
 import upload from "../utils/upload.js";
+import {
+    newComment,
+    getComments,
+    deleteComment,
+} from "../controller/comment-controller.js";
 
 const router = express.Router();
 
@@ -18,5 +23,9 @@ router.post("/update/:id", updatePost);
 router.delete("/delete/:id", deletePost);
 router.post("/file/upload", upload.single("file"), uploadImage);
 router.get("/file/:filename", getImage);
+
+router.post("/comment/new", newComment);
+router.get("/comments/:id", getComments);
+router.delete("/comment/delete/:id", deleteComment);
 
 export default router;
